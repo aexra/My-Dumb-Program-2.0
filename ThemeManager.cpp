f71868062cdd3907713dc27e1ad2b3c44f09d1e4
Никита Фомин <defdXs@yandex.ru>
2023-05-21 14:16:37 +0300

    Описал класс ThemeManager

--- ThemeManager.cpp
+++ ThemeManager.cpp
@@ -1,2 +1,37 @@
 #include "ThemeManager.h"
 
+ThemeManager* ThemeManager::p_instance = nullptr;
+
+ThemeManager::ThemeManager()
+{
+
+}
+
+ThemeManager* ThemeManager::GetInstance()
+{
+	if (p_instance)
+		return p_instance;
+	return (p_instance = new ThemeManager());
+}
+
+void ThemeManager::SetLight()
+{
+	palette = light;
+}
+
+void ThemeManager::SetDark()
+{
+	palette = dark;
+}
+
+BOOL ThemeManager::GetTheme()
+{
+	return theme;
+}
+
+PALETTE ThemeManager::GetPalette()
+{
+	return palette;
+}
+
+
--- ThemeManager.h
+++ ThemeManager.h
@@ -1,9 +1,46 @@
 #pragma once
 
 #include "Header.h"
+#include "Vector3.h"
+
+struct PALETTE
+{
+	V3 bk;
+	V3 bd;
+	V3 text;
+	V3 fbk;
+	V3 fbd;
+	V3 link;
+	V3 vbk;
+	V3 vbd;
+}
+dark {
+	V3(20, 20, 20),
+	V3(36, 35, 35),
+	V3(255, 255, 255),
+	V3(33, 31, 31),
+	V3(46, 43, 43),
+	V3(135, 127, 127),
+	V3(94, 91, 91),
+	V3(255, 255, 255)
+},
+light{
+
+}
+;
 
 class ThemeManager
 {
-
+private:
+	static ThemeManager* p_instance;
+	BOOL theme = 0; // 0 = dark, 1 = light
+	PALETTE palette = (theme == 0? dark : light);
+	ThemeManager();
+public:
+	ThemeManager* GetInstance();
+	void SetLight();
+	void SetDark();
+	BOOL GetTheme();
+	PALETTE GetPalette();
 };
 
