c8ac62724bae30e482866959e46257c825350299
Никита Фомин <defdXs@yandex.ru>
2023-05-22 20:59:04 +0300

    Ура он начал делать дисконнект

--- Edge.cpp
+++ Edge.cpp
@@ -1,7 +1,47 @@
 #include "Edge.h"
 
+vector<Edge> Edge::edges = { };
+
 Edge::Edge(Vertex* _Begin, Vertex* _End)
 {
 	begin = _Begin;
 	end = _End;
+	RecalcPosition();
+}
+
+POINT Edge::RecalcPosition()
+{
+	crossPos.x = abs(begin->GetCenter().x - end->GetCenter().x);
+	crossPos.y = abs(begin->GetCenter().y - end->GetCenter().y);
+	return crossPos;
+}
+
+POINT Edge::getpt(BOOL toRecalc)
+{
+	return (toRecalc? RecalcPosition() : crossPos);
+}
+
+bool operator==(const Edge& a, const Edge& b)
+{
+	if (a.begin == b.begin && a.end == b.end || a.begin == b.end && a.end == b.begin)
+		return 1;
+	return 0;
+}
+bool operator!=(const Edge& a, const Edge& b)
+{
+	if (a.begin == b.begin && a.end == b.end || a.begin == b.end && a.end == b.begin)
+		return 0;
+	return 1;
+}
+bool operator==(const Edge& a, const pair<Vertex*, Vertex*>& b)
+{
+	if (a.begin == b.first && a.end == b.second || a.begin == b.second && a.end == b.first)
+		return 1;
+	return 0;
+}
+bool operator!=(const Edge& a, const pair<Vertex*, Vertex*>& b)
+{
+	if (a.begin == b.first && a.end == b.second || a.begin == b.second && a.end == b.first)
+		return 0;
+	return 1;
 }
--- Edge.h
+++ Edge.h
@@ -2,17 +2,26 @@
 
 #include "Header.h"
 #include "Vertex.h"
+#include "Button.h"
 
 class Edge
 {
 private:
 	POINT crossPos;
+	BUTTON* btn;
 public:
 	Vertex* begin;
 	Vertex* end;
 
 	Edge(Vertex* _Begin, Vertex* _End);
-	void RecalcPosition();
-	void DrawDscnctCross();
+	POINT RecalcPosition();
+	POINT getpt(BOOL toRecalc = 0);
+	
+
+	static vector<Edge> edges;
 };
 
+bool operator==(const Edge& a, const Edge& b);
+bool operator!=(const Edge& a, const Edge& b);
+bool operator==(const Edge& a, const pair<Vertex*, Vertex*>& b);
+bool operator!=(const Edge& a, const pair<Vertex*, Vertex*>& b);
